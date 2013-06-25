aaf7fc075b5ef830ab935ef737f729fc9a7bd88a
David Fisher <tibbon+github@gmail.com>
2013-06-25 16:32:03 -0400

    Updated to show posting into files

--- main.rb
+++ main.rb
@@ -6,13 +6,21 @@ get '/' do
   erb :home
 end
 
-get '/manual_links' do
-  @links = ['http://reddit.com',
-            'http://news.ycombinator.com',
-            'http://techcrunch.com',
-            'http://macrumors.com',
-            'http://gizmodo.com']
-  erb :links
+get '/about' do
+  "This is me"
+end
+
+get '/new_link' do
+  erb :new_link
+end
+
+post '/new_link' do
+  @link_name = params[:name]
+  @link = params[:link]
+  f = File.new('links.csv', 'a+')
+  f.puts("#{@link_name},#{@link}")
+  f.close
+  redirect to("/links")
 end
 
 get '/links' do
@@ -24,3 +32,27 @@ get '/links' do
   link_file.close
   erb :links
 end
+
+post '/navigate' do
+  case params[:destination].downcase
+    when "links" then redirect to("/links")
+    when "about" then redirect to ("/about")
+  end
+  redirect to("/")
+end
+
+
+
+
+
+
+
+
+# get '/manual_links' do
+#   @links = ['http://reddit.com',
+#             'http://news.ycombinator.com',
+#             'http://techcrunch.com',
+#             'http://macrumors.com',
+#             'http://gizmodo.com']
+#   erb :links
+# end
